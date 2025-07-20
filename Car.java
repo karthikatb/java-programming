fdfd9bb8784645d38ee9ec5334c1d867ce2852ac
karthikatb <karthikatb20@gmail.com>
2025-07-20 14:36:53 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Car {
+	String color;
+	String model;
+	void drive() {
+		System.out.println("The car is running");
+	}
+	void stop() {
+		System.out.println("The car has stopped");
+	}
+	
+}
+
