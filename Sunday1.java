fe855be9dc47fb263ae717510d233f10ec65110e
karthikatb <karthikatb20@gmail.com>
2025-07-20 15:40:43 +0530

    Add files via upload

---
+++ Sunday1.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Sunday1 {
+	String name;
+	String dep;
+	int roll;
+	void details() {
+		System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll);
+        System.out.println("Department: " + dep);
+	}
+}
